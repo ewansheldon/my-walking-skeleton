47c5bc7ba7d1657d675a122d39fc8fa7d8defd29
Ewan <ewan.s@cucumberwifi.io>
2020-03-19 10:57:30 +0000

    fail test

--- src/test/java/codurance/AppTest.java
+++ src/test/java/codurance/AppTest.java
@@ -7,8 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class AppTest {
     @Test
     void run_this_test_and_pass() {
-        System.out.println("this test was run !");
-        System.out.println("this test was really run !");
         assertTrue(true);
     }
 
